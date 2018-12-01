377dd31b768b326f1c79b7eb5be36d22a26cca8b
Gerad <34050016+sirantes@users.noreply.github.com>
2018-12-01 15:43:03 -0800

    nop

---
+++ NOP/NOP.srcs/sources_1/new/CS151_NOP.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/01/2018 03:42:15 PM
+// Design Name: 
+// Module Name: CS151_NOP
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CS151_NOP(
+
+    );
+endmodule
