1bfacfd8fd67da3c30d6a49a8c95e12b0c654d41
Gerad <34050016+sirantes@users.noreply.github.com>
2018-12-02 17:51:58 -0800

    sub

--- modules/subtractor/subtractor.srcs/sources_1/new/sub.v
+++ modules/subtractor/subtractor.srcs/sources_1/new/sub.v
@@ -26,17 +26,20 @@ module sub(
     output [31:0]Sum,
     output [1:0]Equal,
     output [1:0]Carry,
-    output [1:0]Overflow
+    output [1:0]Overflow,
+    output [32:0]OverflowCheck,
+    output [31:0] Beta
     );
     wire [32:0] OverflowCheck;
+    wire [31:0] ov;
+    wire[31:0] ba;
     integer OFC;
     integer s;
     integer C;
     assign Sum = A-B;
-    reg [31:0] Beta;
-    assign Beta = ~B;
-    assign Beta[0] = 1'b0;
-
+    assign Beta = ~B + 1'b1;
+    assign OverflowCheck = A+Beta;
+    assign ov = A+Beta;
  always@(*) begin
             if (Sum == 32'b00000000000000000000000000000000 )
                 s = 1;
@@ -44,21 +47,24 @@ module sub(
                 s = 0;
             end
   always@(*) begin
-        if (OverflowCheck !== Sum )
+        if (OverflowCheck[32] == 1'b1 )
             C = 1;
         else
             C = 0;
+        if (A==B)
+            C=0;
         end
     always@(*) begin
-            if (A[31] != B[31])begin
-                if (A[31] != Sum[31] )
+        if (A[31] != B[31])begin
+                if (A[31] != ov[31] )    
                     OFC = 1;
                 else
                     OFC = 0;
                 end
-            else
-                OFC = 0;
-            end
+        else begin
+            OFC = 0;
+                end
+                end
     assign Overflow = OFC;
     assign Carry = C;
     assign Equal = s;
